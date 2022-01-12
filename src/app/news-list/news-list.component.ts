b13d2e6cbe68451a45932798ebc0baf4dc5ddc59
Chitranshi Srivastava <apigeoneer@gmail.com>
2022-01-12 11:49:39 +0530

    display news items fetched

--- src/app/news-list/news-list.component.ts
+++ src/app/news-list/news-list.component.ts
@@ -8,23 +8,29 @@ import { Observable } from 'rxjs';
   styleUrls: ['./news-list.component.css'],
 })
 export class NewsListComponent implements OnInit {
+  public newsResponse = { status: '', totalResults: -1, articles: [] };
   public newsList: any[] = [];
 
   private API_KEY = '53a8148678004285a49eecd885986d61';
 
   private _url: string =
-    'newsapi.org/v2/everything?q=bitcoin&apiKey=' + this.API_KEY;
+    'https://newsapi.org/v2/everything?q=tesla&from=2021-12-12&sortBy=publishedAt&apiKey=' +
+    this.API_KEY;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.getNewsList().subscribe((data) => (this.newsList = data));
+    this.getNewsList().subscribe((data) => (this.newsResponse = data));
+    this.newsList = this.newsResponse.articles;
 
-    console.log(this.newsList);
+    let response = this.getNewsList();
+    console.log('response', response);
+    console.log('newsResponse', this.newsResponse);
+    console.log('newsList', this.newsList);
   }
 
   // Cast the Observable into an array
-  getNewsList(): Observable<any[]> {
-    return this.http.get<any[]>(this._url);
+  getNewsList(): Observable<any> {
+    return this.http.get<any>(this._url);
   }
 }
